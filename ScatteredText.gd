9e953bc1e5e74f4c21daf669e14d471a1e90d6ad
uknable <uknable@hotmail.com>
2019-11-21 23:05:22 +1100

    Initial commit

---
+++ ScatteredText.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+const LINESPACE = 55
+const SELF_PITY = [
+	"I never saw a wild thing",
+	"sorry for itself.",
+	"A small bird will drop frozen dead from a bough",
+	"without ever having felt sorry for itself"
+]
+
---
+++ self-pity.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+func _on_I_gui_input(event):
+	if event is InputEventMouseButton:
+		print("Hello")
