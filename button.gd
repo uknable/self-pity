30b5d3067f000fcd928b684c9da5f283dd881f86
uknable <uknable@hotmail.com>
2019-11-27 19:35:07 +1100

    chage everything

--- button.gd
+++ button.gd
@@ -1,32 +1,32 @@
 extends Button
 
 signal hello
+signal imDie
 
 var down = false
 var size
-var ownDictValue
-var otherDictValue
+var ownLineNum
+var ownWordNum
+var otherLineNum
+var otherWordNum
 var otherButton
+var otherText
 var button_rect
-const SNAP_RADIUS = 10
 
 func _ready():
 	connect("button_down", self, "_on_WordButton_button_down")
 	connect("button_up", self, "_on_WordButton_button_up")
-	connect("hello", self, "_on_hello")
+	connect("imDie", self, "_on_die")
 
 	size = self.get_size()
 	$ButtonArea.set_position(size/2)
 	button_rect = $ButtonArea.get_child(0).get_shape()
 	button_rect.set_extents(size/2)
 
-	
 
 func _process(delta):
 	if (down && Input.is_action_pressed("left_mouse")):
 		self.set_position(get_viewport().get_mouse_position())
-
-
 
 
 func _on_WordButton_button_down():
@@ -35,47 +35,56 @@ func _on_WordButton_button_down():
 
 
 func _on_WordButton_button_up():
-	if (ownDictValue[0] == otherDictValue[0]): # merge
+	if (ownLineNum == otherLineNum): # merge
 		var parent = self.get_parent()
+		var side = ownWordNum - otherWordNum
 
-		if (ownDictValue[1] - otherDictValue[1] == 1):
+		if (side == -1):
+			print(self.text, " was dropped to the left of ", otherText)
+			otherButton.text = self.text + " " + otherButton.text
+			emit_signal("imDie", ownLineNum, ownWordNum)
 
-			print(ownDictValue[2], " was dropped to the right of ", otherDictValue[2])
-			parent.removeButton(otherButton)
-			parent.removeButton(self)
-			
-			parent.changeDict(ownDictValue[0])
 
-		if (ownDictValue[1] - otherDictValue[1] == -1):
-			print(ownDictValue[2], " was dropped to the left of ", otherDictValue[2])
-			parent.removeButton(otherButton)
-			parent.removeButton(self)
-
-			parent.changeDict(ownDictValue[0]) 
-		
-		# lets delete the dictionary entry and make a new one?
+		if (side == 1):
+			print(self.text, " was dropped to the right of ", otherText)
+			otherButton.text = otherButton.text + " " + self.text
+			emit_signal("imDie", ownLineNum, ownWordNum)
 
 	down = false
 
 
 
 func _on_ButtonArea_area_shape_entered(area_id, area, area_shape, self_shape):
-	ownDictValue = word_manager.wordDict[self]
-
 	if (down):
-		emit_signal("hello", area)
+		otherButton = area.get_parent()
+		otherLineNum = otherButton.ownLineNum
+		otherWordNum = otherButton.ownWordNum
+		otherText = otherButton.text
+	
+		print(self.text, ":", ownLineNum, ":", ownWordNum, " is saying hello to ", otherText, ":", otherLineNum, ":", otherWordNum)
+	
+		if (ownLineNum == otherLineNum): # animation
+			if (ownWordNum - otherWordNum == 1):
+				print(self.text, ": ", ownWordNum, " is to the right of ", otherText, ": ", otherWordNum)
+			if (ownWordNum - otherWordNum == -1):
+				print(self.text, ": ", ownWordNum, " is to the left of ", otherText, ": ", otherWordNum)
 
 
 
-func _on_hello(area):
-	otherButton = area.get_parent()
-	otherDictValue = word_manager.wordDict[otherButton]
+func _on_die(deadLineNum, deadWordNum):
+	print(self.text, ":", ownLineNum, ":", ownWordNum)
+	if (ownWordNum > deadWordNum):
+		ownWordNum -= 1
 
-	print(ownDictValue, " is saying hello to ", otherDictValue)
+	queue_free()
 
-	if (ownDictValue[0] == otherDictValue[0]): # animation
-		if (ownDictValue[1] - otherDictValue[1] == 1):
-			print(self, " is to the right of ", otherButton)
-		if (ownDictValue[1] - otherDictValue[1] == -1):
-			print(self, " is to the left of ", otherButton)
 
+
+
+
+
+func _on_ButtonArea_area_shape_exited(area_id, area, area_shape, self_shape):
+	otherButton = 0
+	otherLineNum = 0
+	otherWordNum = 0
+	otherText = 0
