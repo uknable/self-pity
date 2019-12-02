fc3ebecb7b80cbcb1afb000eb87df9baf373c212
uknable <uknable@hotmail.com>
2019-12-02 16:22:13 +1100

    increment

--- Animation.gd
+++ Animation.gd
@@ -1,2 +1,13 @@
 extends Node2D
 
+
+const INC_RATE = 0.5
+
+var inc = 0
+
+func _process(delta):
+    inc += delta * 0.5
+    
+    get_node("Sprite" + str(floor(fmod(inc, 10)))).set_visible(true)
+
+
